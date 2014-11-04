355eaaf384dd265b79870d4c7fefee248fabd162
KMichalski <michalski@uni-koblenz.de>
2014-11-04 21:46:27 +0100

    cleaning up

--- src/executables/01_Renderer-Test/main.cpp
+++ src/executables/01_Renderer-Test/main.cpp
@@ -4,18 +4,16 @@
 #include "GeKo_Graphics/Shader.h"
 #include "GeKo_Graphics/Buffer.hpp"
 
-//Example
-
-/*
-measure the time for a certain number of loops
-*/
 /*
 first example:
 
-we set a window,
-initialize the renderer and load an object,
-set the shaderhandle & give him the path of the shaders
-and renderering the object in the gameloop
+1. per hand we build a quad
+2. we set a window
+3. set the shader with a fragmentshader & vertexshader
+3.1 fragmentShader get an uniform var 
+4. set a renderer
+5. set a buffer with the quad
+6. gameloop
 */
 int main()
 {
@@ -26,7 +24,6 @@ int main()
     vertices.push_back(glm::vec3(0.5, 0.5, 0.0));
     vertices.push_back(glm::vec3(-0.5, 0.5, 0.0));
 
-
     glfwInit();
 
     //our window
@@ -34,10 +31,12 @@ int main()
 
     glewInit();
 
+	//our shader
     VertexShader vs(loadShaderSource(SHADERS_PATH + std::string("/ColorShader/colorShader.vert")));
     FragmentShader fs(loadShaderSource(SHADERS_PATH + std::string("/ColorShader/colorShader.frag")));
-    ShaderProgram p(vs, fs);
-    //our renderer
+    ShaderProgram shader(vs, fs);
+    
+	//our renderer
     OpenGL3Context context;
     Renderer renderer(context);
     Buffer<glm::vec3> buffer(vertices,STATIC_DRAW);
@@ -46,11 +45,12 @@ int main()
     while (!glfwWindowShouldClose(window.getWindow()))
     {
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-        p.bind();
+        shader.bind();
         //FIXME - need proper shader uniform
-        p.sendVec3("color", glm::vec3(0.5,0.2,0.8));
+        shader.sendVec3("color", glm::vec3(0.5,0.2,0.8));
         renderer.draw(buffer);
-        p.unbind();
+        shader.unbind();
+
         glfwSwapBuffers(window.getWindow());
         glfwPollEvents();
     }
