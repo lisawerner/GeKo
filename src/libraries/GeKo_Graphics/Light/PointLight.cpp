7d27ab371693d1c0a3531fb383f2244f32afbf08
KMichalski <michalski@uni-koblenz.de>
2014-11-24 12:18:39 +0100

    added Light

--- src/libraries/GeKo_Graphics/Light/PointLight.cpp
+++ src/libraries/GeKo_Graphics/Light/PointLight.cpp
@@ -1,7 +1,20 @@
+#pragma once
 #include "PointLight.h"
 
 PointLight::PointLight()
 {
+	m_position = glm::vec4(0.0, 0.0, 0.0, 1.0);
+	m_color = glm::vec3(1.0,1.0,1.0);
+	m_isActive = true;
+	m_radius = 0;
+}
+
+PointLight::PointLight(glm::vec4 position, glm::vec3 color, bool isActive, double radius)
+{
+	m_position = position;
+	m_color = color;
+	m_isActive = isActive;
+	m_radius = radius;
 }
 
 PointLight::~PointLight()
--- src/libraries/GeKo_Graphics/Light/PointLight.h
+++ src/libraries/GeKo_Graphics/Light/PointLight.h
@@ -1,10 +1,19 @@
 #pragma once
-#include "Light.h"
+#include "GeKo_Graphics/include.h"
 
-class PointLight : public Light
+class PointLight 
 {
 public:
 	PointLight();
+	PointLight(glm::vec4 position, glm::vec3 color, bool isActive, double radius);
 	~PointLight();
-};
 
+	glm::vec3 m_color;
+	glm::vec4 m_position;
+	bool m_isActive;
+	float m_radius;
+
+	glm::vec3 m_direction = glm::vec3(0.0, 0.0, 0.0);
+	float m_exponent = 0;
+	int m_angle = 0;
+};
