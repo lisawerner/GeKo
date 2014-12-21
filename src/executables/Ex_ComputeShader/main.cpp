32ae484d1f2f4f38c44839d7fc7de958e5d4d922
NicolasKlee <nklee@uni-koblenz.de>
2014-12-21 17:14:14 +0100

    another try

--- src/executables/Ex_ComputeShader/main.cpp
+++ src/executables/Ex_ComputeShader/main.cpp
@@ -23,29 +23,35 @@ int main() {
 	GLuint ssbo;
 	glGenBuffers(1, &ssbo);
 	glBindBuffer(GL_SHADER_STORAGE_BUFFER, ssbo);
-	glBufferData(GL_SHADER_STORAGE_BUFFER, sizeof(test), &test, GL_STATIC_DRAW);
+	glBufferData(GL_SHADER_STORAGE_BUFFER, 10*sizeof(glm::fvec4), NULL, GL_STATIC_DRAW);
+
+	GLint bufMask = GL_MAP_WRITE_BIT | GL_MAP_INVALIDATE_BUFFER_BIT;
+	glm::fvec4* points = (glm::fvec4*)glMapBufferRange(GL_SHADER_STORAGE_BUFFER, 0, 10 * sizeof(glm::fvec4), bufMask);
+	for (int i = 0; i < 10; i++)
+	{
+		points[i].x = 0.0f;
+		points[i].y = 0.0f;
+		points[i].z = 0.0f;
+		points[i].w = 0.0f;
+	}
+	glUnmapBuffer(GL_SHADER_STORAGE_BUFFER);
 
 	//auto buffer_index = glGetProgramResourceIndex(computeShader.handle, GL_SHADER_STORAGE_BLOCK, "Test");
 	//glShaderStorageBlockBinding(computeShader.handle, buffer_index, 0);
 
 	computeShader.bind();
 	glBindBufferBase(GL_SHADER_STORAGE_BUFFER, 0, ssbo);
-
 	
-	std::cout << "HIER MUSS 2 STEHEN:   " << test << std::endl;
-	glDispatchCompute(1, 1, 1);
-	glMemoryBarrier(GL_SHADER_STORAGE_BARRIER_BIT);
-
-
-	//glUnmapBuffer(GL_SHADER_STORAGE_BUFFER);
+	std::cout << "HIER MUSS 0 STEHEN:   " << points[1].x << points[1].y << points[1].z << std::endl;
+	glDispatchCompute(10 / 2, 1, 1);
+	//glMemoryBarrier(GL_SHADER_STORAGE_BARRIER_BIT);
 
 	computeShader.unbind();
 
-	//glMemoryBarrier(GL_SHADER_STORAGE_BARRIER_BIT);
+	std::cout << "HIER MUSS 1 STEHEN:   " << points[1].x << points[1].y << points[1].z << std::endl;
 	
 	/*END*/
 
-	std::cout << "HIER MUSS 99 STEHEN:   " << test << std::endl;
 
 	while (!glfwWindowShouldClose(window))
 	{
