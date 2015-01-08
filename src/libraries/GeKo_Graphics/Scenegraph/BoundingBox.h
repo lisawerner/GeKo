dbc8706b73cc3c9654f2a3df92953a82b931b852
MarcelBock <mb.bock@online.de>
2015-01-08 17:48:25 +0100

    comments update Bounding box

--- src/libraries/GeKo_Graphics/Scenegraph/BoundingBox.h
+++ src/libraries/GeKo_Graphics/Scenegraph/BoundingBox.h
@@ -3,17 +3,23 @@
 #include <glm/ext.hpp>
 #include <GeKo_Graphics/Scenegraph/Node.h>
 
-
+/** The Bounding Box class provides a cube-like box, which contains an Object completely*/
 class BoundingBox {
+
+public:
+	///Each Bounding Box needs a Object to wrap
+	/**/
+	BoundingBox(Node* object);
+	~BoundingBox();
+
+	///Returns all points of the Box
+	/**/
+	std::vector<glm::vec4> boundingBox(Node* object);
 
 private:
     std::vector<glm::vec4> vertices;
 
-public:
-    BoundingBox(Node* object);
-	~BoundingBox();
-	
-	std::vector<glm::vec4> boundingBox(Node* object);
+
 
 
 };
