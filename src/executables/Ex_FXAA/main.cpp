91651ebef5a92a47ff752331e1f64c62e882b778
JanBTG <jan@a-beutgen.info>
2015-01-15 16:36:04 +0100

    Anti Aliasing

---
+++ src/executables/Ex_FXAA/main.cpp
@@ -0,0 +1,163 @@
+#include <GL/glew.h>
+#include <GeKo_Graphics/InputInclude.h>
+#include <GeKo_Graphics/MaterialInclude.h>
+#include <GeKo_Graphics/ObjectInclude.h>
+#include <GeKo_Graphics/ShaderInclude.h>
+#include <GeKo_Graphics/ScenegraphInclude.h>
+#include "GeKo_Graphics/Camera/Pilotview.h"
+
+const int WINDOW_WIDTH = 800;
+const int WINDOW_HEIGHT = 600;
+
+bool USE_FXAA = true;
+
+InputHandler iH;
+Pilotview cam("Pilotview");
+
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods){
+	std::map<int, std::function<void()>> activeMap = iH.getActiveInputMap()->getMap();
+
+	if (key == GLFW_KEY_F1)
+		USE_FXAA = true;
+	
+	if (key == GLFW_KEY_F2)
+		USE_FXAA = false;
+
+	for (std::map<int, std::function<void()>>::iterator it = activeMap.begin(); it != activeMap.end(); it++){
+		if (it->first == key)
+			activeMap.at(key)();
+		if (it == activeMap.end())
+			std::cout << "Key is not mapped to an action" << std::endl;
+	}
+}
+
+int main()
+{
+	glfwInit();
+
+	Window testWindow(50, 50, WINDOW_WIDTH, WINDOW_HEIGHT, "FXAA");
+	glfwMakeContextCurrent(testWindow.getWindow());
+	glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+	
+	// You have to set a camera name
+	cam.setName("PilotviewCam");
+	cam.setPosition(glm::vec4(0.0, 0.3, 1.0, 1.0));
+	cam.setNearFar(0.001f, 100.0f);
+
+	iH.setAllInputMaps(cam);
+	iH.changeActiveInputMap("Pilotview");
+
+	//Callback
+	glfwSetKeyCallback(testWindow.getWindow(), key_callback);
+
+	glewInit();
+
+	//our shader
+	VertexShader vsGBuffer(loadShaderSource(SHADERS_PATH + std::string("/GBuffer/GBuffer.vert")));
+	FragmentShader fsGBuffer(loadShaderSource(SHADERS_PATH + std::string("/GBuffer/GBuffer.frag")));
+	ShaderProgram shaderGBuffer(vsGBuffer, fsGBuffer);
+
+	VertexShader vsFXAA(loadShaderSource(SHADERS_PATH + std::string("/FXAA/FXAA.vert")));
+	FragmentShader fsFXAA(loadShaderSource(SHADERS_PATH + std::string("/FXAA/FXAA.frag")));
+	ShaderProgram shaderFXAA(vsFXAA, fsFXAA);
+
+	VertexShader vsSfq(loadShaderSource(SHADERS_PATH + std::string("/ScreenFillingQuad/ScreenFillingQuad.vert")));
+	FragmentShader fsSfq(loadShaderSource(SHADERS_PATH + std::string("/ScreenFillingQuad/ScreenFillingQuad.frag")));
+	ShaderProgram shaderSFQ(vsSfq, fsSfq);
+
+	//our renderer
+	OpenGL3Context context;
+	Renderer renderer(context);
+
+	FBO fboGBuffer(WINDOW_WIDTH, WINDOW_HEIGHT, 3, true, false);
+	FBO fboFXAA(WINDOW_WIDTH, WINDOW_HEIGHT, 1, false, false);
+
+	//our object
+	Teapot teapot;
+	teapot.loadBufferData();
+
+	Rect screenFillingQuad;
+	screenFillingQuad.loadBufferData();
+
+	//our textures
+	Texture chrome((char*)RESOURCES_PATH "/chrome.jpg");
+
+	//Scene creation 
+	Level testLevel("testLevel");
+	Scene testScene("testScene");
+	testLevel.addScene(testScene);
+	testLevel.changeScene("testScene");
+
+	//Add Camera to scenegraph
+	testScene.getScenegraph()->addCamera(&cam);
+	testScene.getScenegraph()->getCamera("PilotviewCam");
+	testScene.getScenegraph()->setActiveCamera("PilotviewCam");
+
+
+	Node teapotNode("teapot");
+	teapotNode.addGeometry(&teapot);
+	teapotNode.addTexture(&chrome);
+	teapotNode.setModelMatrix(glm::scale(teapotNode.getModelMatrix(), glm::vec3(0.2, 0.2, 0.2)));
+
+
+
+	//Creating a scenegraph
+	testScene.getScenegraph()->getRootNode()->addChildrenNode(&teapotNode);
+
+	double startTime = glfwGetTime();
+	//Renderloop
+
+
+	while (!glfwWindowShouldClose(testWindow.getWindow()))
+	{
+		// You have to compute the delta time
+		cam.setSensitivity(glfwGetTime() - startTime);
+		
+		startTime = cam.getSensitivity();
+
+		fboGBuffer.bind();
+		glClearColor(0, 0, 0, 0);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+		shaderGBuffer.bind();
+		shaderGBuffer.sendMat4("viewMatrix", cam.getViewMatrix());
+		shaderGBuffer.sendMat4("projectionMatrix", cam.getProjectionMatrix());
+		shaderGBuffer.sendInt("useTexture", 1);
+		testScene.render(shaderGBuffer);
+		shaderGBuffer.unbind();
+		fboGBuffer.unbind();
+
+		fboFXAA.bind();
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+		shaderFXAA.bind();
+
+		shaderFXAA.sendSampler2D("colorTexture", fboGBuffer.getColorTexture(2), 2);
+		shaderFXAA.sendInt("fboWidth", WINDOW_WIDTH);
+		shaderFXAA.sendInt("fboHeight", WINDOW_HEIGHT);
+
+		screenFillingQuad.renderGeometry();
+		
+		shaderFXAA.unbind();
+		fboFXAA.unbind();
+				
+		//ScreenFillingQuad Render Pass
+		shaderSFQ.bind();
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+		if (USE_FXAA)
+			shaderSFQ.sendSampler2D("texture", fboFXAA.getColorTexture(0));
+		else
+			shaderSFQ.sendSampler2D("texture", fboGBuffer.getColorTexture(2));
+
+		screenFillingQuad.renderGeometry();
+		shaderSFQ.unbind();
+		
+		
+		glfwSwapBuffers(testWindow.getWindow());
+		glfwPollEvents();
+	}
+
+	glfwDestroyWindow(testWindow.getWindow());
+	glfwTerminate();
+
+	return 0;
+}
---
+++ src/shaders/FXAA/FXAA.frag
@@ -0,0 +1,56 @@
+#version 330
+
+in vec2 passUVCoord;
+
+uniform sampler2D colorTexture;
+
+uniform int fboWidth;
+uniform int fboHeight;
+
+layout(location = 0) out vec4 colorOutput;
+
+void main()
+{
+	float FXAA_SPAN_MAX = 8.0;
+	float FXAA_REDUCE_MIN = 1.0/128.0;
+	float FXAA_REDUCE_MUL = 1.0/8.0;
+	
+	//get luminosity of surrounding pixels
+	vec3 luma = vec3(0.299,0.587,0.114); //official values for getting luminosity value of a color
+	
+	vec2 texCoordOffset = vec2(1.0/fboWidth,1.0/fboHeight);
+	
+	float lumaTL	=	dot(luma,texture(colorTexture,passUVCoord.xy + (vec2(-1.0,-1.0) * texCoordOffset)).xyz);
+	float lumaTR	=	dot(luma,texture(colorTexture,passUVCoord.xy + (vec2(1.0,-1.0) * texCoordOffset)).xyz);
+	float lumaBL	=	dot(luma,texture(colorTexture,passUVCoord.xy + (vec2(-1.0,1.0) * texCoordOffset)).xyz);
+	float lumaBR	=	dot(luma,texture(colorTexture,passUVCoord.xy + (vec2(1.0,1.0) * texCoordOffset)).xyz);
+	float lumaM		=	dot(luma,texture(colorTexture,passUVCoord.xy).xyz);
+
+	//calculate Blur direction - direction is zero if there is no edge here, because luminosity is similiar in this case
+	vec2 dir;
+	dir.x = -((lumaTL + lumaTR) - (lumaBL + lumaBR));
+	dir.y =  ((lumaTL + lumaBL) - (lumaTR + lumaBR));
+	
+	float dirReduce = max((lumaTL + lumaTR + lumaBL + lumaBR)*(FXAA_REDUCE_MUL * 0.25),FXAA_REDUCE_MIN); //prevents division by zero
+	float inverseDirAdjustment = 1.0/(min(abs(dir.x), abs(dir.y)) + dirReduce);
+	
+	dir = min(vec2(FXAA_SPAN_MAX,FXAA_SPAN_MAX), max(vec2(-FXAA_SPAN_MAX,-FXAA_SPAN_MAX), dir * inverseDirAdjustment)) * texCoordOffset;
+	
+	vec3 result1 = (1.0/2.0) * (
+	texture(colorTexture,passUVCoord.xy  + (dir * vec2(1.0/3.0 - 0.5))).xyz +
+	texture(colorTexture,passUVCoord.xy  + (dir * vec2(2.0/3.0 - 0.5))).xyz);
+	
+	vec3 result2 = result1* (1.0/2.0) + (1.0/4.0) * (
+	texture(colorTexture,passUVCoord.xy  + (dir * vec2(0.0/3.0 - 0.5))).xyz +
+	texture(colorTexture,passUVCoord.xy  + (dir * vec2(3.0/3.0 - 0.5))).xyz);
+	
+	//test if result2 was too far sampled. if so - take result1 as result
+	float lumaMin = min(lumaM, min(min(lumaTL, lumaTR), min(lumaBL,lumaBR)));
+	float lumaMax = max(lumaM, max(max(lumaTL, lumaTR), max(lumaBL,lumaBR)));
+	float lumaResult2 = dot(luma,result2);
+	
+	if(lumaResult2 < lumaMin || lumaResult2 > lumaMax)
+		colorOutput = vec4(result1,1.0);
+	else
+		colorOutput = vec4(result2,1.0);	
+}
