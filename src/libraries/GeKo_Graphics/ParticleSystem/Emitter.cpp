01a5b88f33c3b133413415a7737cc3f75ee690fe
KMichalski <michalski@uni-koblenz.de>
2015-04-09 10:13:19 +0200

    some fixes

--- src/libraries/GeKo_Graphics/ParticleSystem/Emitter.cpp
+++ src/libraries/GeKo_Graphics/ParticleSystem/Emitter.cpp
@@ -13,12 +13,6 @@ Emitter::Emitter(const int OUTPUT, glm::vec3 position, double emitterLifetime, d
 	setEmitterMortality(emitterLifetime);
 
 	//set properties for the emitting
-	if (emitFrequency > particleLifeTime){
-		std::string emitFrequencyString = std::to_string(emitFrequency);
-		std::string particleLifetimeString = std::to_string(particleLifetime);
-		perror("Error in Emitter: emitFrequency  > particleLifetime");
-		std::cout << "ef:" << emitFrequency << "pt:" << particleLifetimeString << std::endl;
-	}
 	setEmitFrequency(emitFrequency);
 	setParticlesPerEmit(particlesPerEmit);
 
@@ -93,200 +87,25 @@ void Emitter::loadBuffer(){
 	glUnmapBuffer(GL_SHADER_STORAGE_BUFFER);
 	glBindBuffer(GL_SHADER_STORAGE_BUFFER, 0);
 
-	//TODO: Maybe something else than SSBO. Just 2 of 4
 	glGenBuffers(1, &angle_ssbo);
 	glBindBuffer(GL_SHADER_STORAGE_BUFFER, angle_ssbo);
 	glBufferData(GL_SHADER_STORAGE_BUFFER, numMaxParticle * sizeof(glm::vec4), NULL, GL_DYNAMIC_DRAW);
-	glm::vec2* angle = (glm::vec2*) glMapBufferRange(GL_SHADER_STORAGE_BUFFER, 0, numMaxParticle * sizeof(glm::vec4), bufMask);
+	glm::vec4* angle = (glm::vec4*) glMapBufferRange(GL_SHADER_STORAGE_BUFFER, 0, numMaxParticle * sizeof(glm::vec4), bufMask);
 	for (int i = 0; i < numMaxParticle; i++)
 	{
 		angle[i].x = 0.0f;
 		angle[i].y = 0.0f;
+		angle[i].z = 0.0f;
+		angle[i].w = 0.0f;
+
 	}
 	glUnmapBuffer(GL_SHADER_STORAGE_BUFFER);
 	glBindBuffer(GL_SHADER_STORAGE_BUFFER, 0);
-}
-
-void Emitter::update(){
-
-	deltaTime = glfwGetTime() - updateTime; //time remain since last update
-	updateTime = glfwGetTime();
-
-	emitLifetime -= deltaTime;
-	if (emitLifetime < 0 && emitterMortal){
-		m_output = UNUSED;
-	}
-
-	//Bind CS and all SSBO's
-	compute->bind();
-	glBindBufferBase(GL_SHADER_STORAGE_BUFFER, 0, position_ssbo);
-	glBindBufferBase(GL_SHADER_STORAGE_BUFFER, 1, velocity_ssbo);
-	glBindBufferBase(GL_SHADER_STORAGE_BUFFER, 2, angle_ssbo);
-
-	//Uniform Vars
-	compute->sendFloat("deltaTime", (float)deltaTime);
-	compute->sendVec3("emitterPos", getPosition()); //can be saved position, or parameter
-	compute->sendFloat("fullLifetime", particleLifetime);
-	compute->sendInt("particleMortal", m_particleMortal);
-
-	compute->sendVec4("gravity", m_gravity);
-	compute->sendFloat("gravityRange", m_gravityRange);
-	compute->sendInt("gravityFunc", m_gravityFunction);
-	
-	compute->sendInt("useTrajectory", m_useTrajectory);
-	compute->sendInt("useDirectionGravity", m_useDirectionGravity);
-	compute->sendInt("usePointGravity", m_usePointGravity);
-	compute->sendInt("useChaoticSwarmMotion", m_useChaoticSwarmMotion);
-	
-	compute->sendInt("useMovementVertical", m_movementVertical);
-	compute->sendInt("useMovementHorizontalX", m_movementHorizontalX);
-	compute->sendInt("useMovementHorizontalZ", m_movementHorizontalZ);
-
-	//Unbind CD and all SSBO's
-	glDispatchCompute(computeGroupCount, 1, 1); //?
-	glBindBufferBase(GL_SHADER_STORAGE_BUFFER, 0, 0);
-	glBindBufferBase(GL_SHADER_STORAGE_BUFFER, 1, 0);
-	glBindBufferBase(GL_SHADER_STORAGE_BUFFER, 2, 0);
-	glMemoryBarrier(GL_VERTEX_ATTRIB_ARRAY_BARRIER_BIT);
-	compute->unbind();
-}
-void Emitter::pushParticle(int numberNewParticle){
-	auto emitPosition = getPosition();
-	auto areaEmittingXY = getAreaEmittingXY();
-	auto areaEmittingXZ = getAreaEmittingXZ();
-
-	glBindBuffer(GL_SHADER_STORAGE_BUFFER, position_ssbo);
-	GLint bufMask = GL_MAP_WRITE_BIT | GL_MAP_INVALIDATE_BUFFER_BIT;
-
-	glm::vec4* positions = (glm::vec4*) glMapBufferRange (GL_SHADER_STORAGE_BUFFER, 0, numMaxParticle * sizeof(glm::vec4), bufMask);
-	if (areaEmittingXY && areaEmittingXZ){//will be emitted in a 3d area
-		auto accuracy = getAreaAccuracy(); //how near it will be generated
-		auto areaSize = getAreaSize(); //how big the area is
-		float randomNumber;
-		glm::vec3 pos;
-
-		for (int i = 0; i < numberNewParticle; i++)
-		{
-			int index = (indexBuffer + i) % numMaxParticle;
-
-			randomNumber = (rand() % (2 * accuracy + 1) - accuracy) / (float)accuracy; //-1 .. 1 with a certain comma accuracy
-			pos.x = emitPosition.x + areaSize * randomNumber;
-			randomNumber = (rand() % (2 * accuracy + 1) - accuracy) / (float)accuracy; //-1 .. 1
-			pos.y = emitPosition.y + areaSize * randomNumber;
-			randomNumber = (rand() % (2 * accuracy + 1) - accuracy) / (float)accuracy; //-1 .. 1
-			pos.z = emitPosition.z + areaSize * randomNumber;
-
-			positions[index] = glm::vec4(pos, particleLifetime);
-		}
-	}
-	else if (areaEmittingXZ){ //emits in a xz area, like rain
-		auto accuracy = getAreaAccuracy(); //how near it will be generated
-		auto areaSize = getAreaSize(); //how big the area is
-		float randomNumber;
-		glm::vec3 pos;
-
-		for (int i = 0; i < numberNewParticle; i++)
-		{
-			int index = (indexBuffer + i) % numMaxParticle;
-			
-			randomNumber = (rand() % (2 * accuracy + 1) - accuracy) / (float) accuracy; //-1 .. 1 with a certain comma accuracy
-			pos.x = emitPosition.x + areaSize * randomNumber;
-			randomNumber = (rand() % (2 * accuracy + 1) - accuracy) / (float) accuracy; //-1 .. 1
-			pos.z = emitPosition.z + areaSize * randomNumber;
-			pos.y = emitPosition.y;
-
-			positions[index] = glm::vec4(pos, particleLifetime);
-		}
-	}
-	else if (areaEmittingXY){ //will be emitted in xy area
-		auto accuracy = getAreaAccuracy();
-		auto areaSize = getAreaSize();
-		float randomNumber;
-		glm::vec3 pos;
-
-		for (int i = 0; i < numberNewParticle; i++)
-		{
-			int index = (indexBuffer + i) % numMaxParticle;
-
-			randomNumber = (rand() % (2 * accuracy + 1) - accuracy) / (float)accuracy; //-1 .. 1 with a certain comma accuracy
-			pos.x = emitPosition.x + areaSize * randomNumber;
-			randomNumber = (rand() % (2 * accuracy + 1) - accuracy) / (float)accuracy; //-1 .. 1
-			pos.y = emitPosition.y + areaSize * randomNumber;
-			pos.z = emitPosition.z;
-
-			positions[index] = glm::vec4(pos, particleLifetime);
-		}
-	}
-	else{ //will be emitted like a jet
-		for (int i = 0; i < numberNewParticle; i++)
-		{
-			int index = (indexBuffer + i) % numMaxParticle;
-			positions[index] = glm::vec4(emitPosition, particleLifetime);
-		}
-	}
-
-	glUnmapBuffer(GL_SHADER_STORAGE_BUFFER);
-	glBindBuffer(GL_SHADER_STORAGE_BUFFER, 0);
-	
-	auto speed = getSpeed();
-	
-	glBindBuffer(GL_SHADER_STORAGE_BUFFER, velocity_ssbo);
-	
-	glm::vec4* velocity = (glm::vec4*) glMapBufferRange (GL_SHADER_STORAGE_BUFFER, 0, numMaxParticle * sizeof(glm::vec4), bufMask); //direction of movement
-	for (int i = 0; i < numberNewParticle; i++)
-	{
-		glm::vec3 vel = this->m_pfunc(); //gets a random vector from the desired vector space
-		if (glm::length(vel) != 0.0)
-			vel = glm::normalize(vel);
-		int index = (indexBuffer + i) % numMaxParticle;
-		velocity[index] = glm::vec4(vel, speed);
-	}
-
-	glUnmapBuffer(GL_SHADER_STORAGE_BUFFER);
-	glBindBuffer(GL_SHADER_STORAGE_BUFFER, 0);
-
-	//TODO just 2 of 4
-	glBindBuffer(GL_SHADER_STORAGE_BUFFER, angle_ssbo);
-	
-	glm::vec2* angle = (glm::vec2*) glMapBufferRange(GL_SHADER_STORAGE_BUFFER, 0, numMaxParticle * sizeof(glm::vec4), bufMask); //angle of movement as option to velocity
-	for (int i = 0; i < numberNewParticle; i++)
-	{
-		int index = (indexBuffer + i) % numMaxParticle;
-		int phi = (rand() % 360);
-		int theta = (rand() % 90);
-		angle[index] = glm::vec2(phi, theta);
-	}
-	glUnmapBuffer(GL_SHADER_STORAGE_BUFFER);
-	glBindBuffer(GL_SHADER_STORAGE_BUFFER, 0);
-
-	indexBuffer = (indexBuffer + numberNewParticle) % numMaxParticle;
-}
-void Emitter::generateParticle()
-{
-	switch (m_output)
-	{
-	case CONSTANT: //we generate constant new particle
-		if (glfwGetTime() - generateTime >= emitFrequency){
-			deltaTime = glfwGetTime() - generateTime;
-			while (deltaTime >= emitFrequency) {
-				pushParticle(particlesPerEmit);
-				deltaTime -= emitFrequency;
-			}
-			generateTime = glfwGetTime();
-		}
-		break;
-	case ONCE: //we generate only one time particle
-		if (glfwGetTime() - generateTime >= emitFrequency){
-			pushParticle(particlesPerEmit);
-			m_output = UNUSED;
-		}
-		break;
-	case UNUSED: // we dont generate new particle
-		break;
-	}
 }
 
 void Emitter::update(glm::vec3 playerPosition){
+
+	generateParticle(playerPosition);
 
 	deltaTime = glfwGetTime() - updateTime; //time remain since last update
 	updateTime = glfwGetTime();
@@ -424,16 +243,15 @@ void Emitter::pushParticle(int numberNewParticle, glm::vec3 playerPosition){
 	glUnmapBuffer(GL_SHADER_STORAGE_BUFFER);
 	glBindBuffer(GL_SHADER_STORAGE_BUFFER, 0);
 
-	//TODO just 2 of 4
 	glBindBuffer(GL_SHADER_STORAGE_BUFFER, angle_ssbo);
 
-	glm::vec2* angle = (glm::vec2*) glMapBufferRange(GL_SHADER_STORAGE_BUFFER, 0, numMaxParticle * sizeof(glm::vec4), bufMask); //angle of movement as option to velocity
+	glm::vec4* angle = (glm::vec4*) glMapBufferRange(GL_SHADER_STORAGE_BUFFER, 0, numMaxParticle * sizeof(glm::vec4), bufMask); //angle of movement as option to velocity
 	for (int i = 0; i < numberNewParticle; i++)
 	{
 		int index = (indexBuffer + i) % numMaxParticle;
-		int phi = (rand() % 360);
-		int theta = (rand() % 90);
-		angle[index] = glm::vec2(phi, theta);
+		int phi = 50.0; // (rand() % 360); //x&z axis
+		int theta = 50; //y axis
+		angle[index] = glm::vec4(phi, theta, 0.0, 0.0);
 	}
 	glUnmapBuffer(GL_SHADER_STORAGE_BUFFER);
 	glBindBuffer(GL_SHADER_STORAGE_BUFFER, 0);
@@ -534,7 +352,7 @@ void Emitter::render(Camera &cam)
 		emitterShader->unbind();
 	}
 	else if (getUseGeometryShader() && useTexture){
-		glDepthFunc(GL_FALSE);
+		glDepthMask(GL_FALSE);
 		glEnable(GL_BLEND);
 		glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 
@@ -585,7 +403,7 @@ void Emitter::render(Camera &cam)
 
 		glDisableClientState(GL_VERTEX_ARRAY);
 		glBindBuffer(GL_ARRAY_BUFFER, 0);
-		glDepthFunc(GL_TRUE);
+		glDepthMask(GL_TRUE);
 		glDisable(GL_BLEND);
 		emitterShader->unbind();
 	}
@@ -666,7 +484,7 @@ void Emitter::usePhysicPointGravity(glm::vec4 gravity, float gravityRange, int g
 	m_gravityFunction = gravityFunction;
 	m_gravityRange = gravityRange;
 }
-void Emitter::usePhysicSwarmCircleMotion(bool movementVertical, bool movementHorizontalX, bool movementHorizontalZ){
+void Emitter::usePhysicSwarmCircleMotion(bool movementVertical, bool movementHorizontalX, bool movementHorizontalZ, float speed){
 	m_useTrajectory = false;
 	m_useDirectionGravity = false;
 	m_usePointGravity = false;
@@ -675,6 +493,8 @@ void Emitter::usePhysicSwarmCircleMotion(bool movementVertical, bool movementHor
 	m_movementVertical = movementVertical;
 	m_movementHorizontalX = movementHorizontalX;
 	m_movementHorizontalZ = movementHorizontalZ;
+
+	setSpeed(speed);
 }
 
 //setters:
@@ -771,7 +591,7 @@ void Emitter::addTexture(Texture* texture, float time){
 	}
 }
 
-void Emitter::useTexture(bool useTexture, float particleSize, float birthTime, float deathTime, float blendingTime, bool rotateLeft, float rotationSpeed){
+void Emitter::defineLook(bool useTexture, float particleSize, float birthTime, float deathTime, float blendingTime, bool rotateLeft, float rotationSpeed){
 	m_useTexture = useTexture;
 
 	m_birthTime = birthTime;
@@ -783,7 +603,7 @@ void Emitter::useTexture(bool useTexture, float particleSize, float birthTime, f
 
 	particleDefaultSize = particleSize;
 }
-void Emitter::useTexture(bool useTexture, std::vector<float> scalingSize, std::vector<float> scalingMoment, 
+void Emitter::defineLook(bool useTexture, std::vector<float> scalingSize, std::vector<float> scalingMoment,
 	float birthTime, float deathTime, float blendingTime, bool rotateLeft, float rotationSpeed){
 	m_useTexture = useTexture;
 	
