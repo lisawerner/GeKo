aac8c81d27caf83a5741493f3d33e937ceee8c65
JanBTG <jan@a-beutgen.info>
2015-04-17 23:50:21 +0200

    Added Doxygen comments to the GUI

--- src/libraries/GeKo_Graphics/GUI/GUI.h
+++ src/libraries/GeKo_Graphics/GUI/GUI.h
@@ -18,23 +18,54 @@ class GUI
     GUI(std::string windowName, int windowWidth = 200, int windowHeight = 100);
     ///Destructor
     ~GUI();
- 
+    
+    ///renders the GUI into to the given window
     void render(Window& window);
+
+    ///adds an element to the GUI
+    /**See GUIComponents.hpp for an overview ovver different GUI Elements that are supported!*/
     void addElement(GuiElement::Element *comp);
+
+    ///Adds a nested window to the current GUI
+    /**The additional window can be shown or hidden. Use a GUI Button to toggle it (See example HUD) !*/
     void addNestedWindow(GuiElement::NestedWindow *nestedWindow);
+
+    ///method to set the GUI to visible
     void show();
+
+    ///method to set the GUI to invisible
     void hide();
+
+    ///getter for GUI visibility
     bool visible();
+
+    ///method to set the alpha value of the GUI background
     void setBackgroundAlpha(float a);
+    
+    ///sets the position of the window on the screen
     void setPosition(float posX, float posY);
+
+    ///changes the visibility of the titlebar
     void setTitleBarVisible(bool visible);
+
+
+    ///changes the ability to move the GUI around
     void setMoveable(bool moveable);
+
+    ///sets the GUI border to visible / invisible
     void setBorderVisible(bool visible);
+
+    ///changes the ability to resize the GUI window
     void setResizable(bool resize);
+
+    ///allows using the GUI scrollbar, if the window gets to large
     void setUseScrollbar(bool scrollbar);
+
+    ///allows to collapse the GUI window
     void setCollapsable(bool collapse);
     
 	std::string m_windowName;
+
   protected:
     
     void init();
@@ -43,8 +74,7 @@ class GUI
 
     bool m_initialized;
     bool m_visible;
-
-   
+       
     int m_windowWidth, m_windowHeight;
     float m_windowPosX, m_windowPosY;
     float m_bgAlpha;
