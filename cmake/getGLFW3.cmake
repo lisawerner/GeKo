50a7444af9932c582bfa6bd1b807fc3641603065
JanBTG <jan@a-beutgen.info>
2015-05-28 09:00:44 +0200

    Cmake GLFW Linux fix

--- cmake/getGLFW3.cmake
+++ cmake/getGLFW3.cmake
@@ -3,13 +3,13 @@ set(glfw_INSTALL_DIR "${DEPENDENCIES_PATH}/glfw")
 set(glfw_CMAKE_ARGS -DCMAKE_INSTALL_PREFIX=${glfw_INSTALL_DIR})   
 	   
 ExternalProject_Add (
-    glfw
+    	glfw
 	PREFIX ${glfw_PREFIX}
-    GIT_REPOSITORY "https://github.com/glfw/glfw"
-    GIT_TAG "master"
+    	GIT_REPOSITORY "https://github.com/glfw/glfw"
+    	GIT_TAG "3.1.1"
 	INSTALL_DIR ${glfw_INSTALL_DIR}
-    UPDATE_COMMAND ""
-    INSTALL_COMMAND "" 
+    	UPDATE_COMMAND ""
+    	INSTALL_COMMAND "" 
 	CMAKE_ARGS ${glfw_CMAKE_ARGS}
 )
 
